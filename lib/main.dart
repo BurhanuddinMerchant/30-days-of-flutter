1d8a193fe2679967c5ed7ec2d5b0b2a538d360ab
Burhanuddin Merchant <bmerchant945@gmail.com>
2021-10-30 15:40:30 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_application/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,10 @@ class MyApp extends StatelessWidget {
       // home: HomePage(),
       title: 'Named Routes Demo',
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
